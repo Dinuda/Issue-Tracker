9252e44495c053c284e89e3fe332fa1303b0d30d
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-03-04 08:36:49 +0530

    add list service

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const yargs = require('yargs')
 // Version Control
 yargs.version('1.1.0')
 
+yargs.command({
+    command: 'list .',
+    describe: 'List all issues',
+    handler: function () {
+        // function
+    }
+})
---
+++ service/list.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const getIssues = function () {
+    console.log("Your Issues: ");
+}
+
+// Load all the notes
+const loadIssues = function () {
+    try {
+        const dataBuffer = fs.readFileSync('issues.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+// Export the functions to the main file(app.js)
+module.exports = {
+    getIssues: getIssues,
+    loadIssues: loadIssues
+}
